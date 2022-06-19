05fe6fd5b7aa93c2169f84a9667e938c65534fa3
myth@mac <mithunkarthick3@gmail.com>
2022-06-19 16:13:10 +0530

    trying bcrypt

--- login/db/mongo-middlewares.js
+++ login/db/mongo-middlewares.js
@@ -1,8 +1,8 @@
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 const encrypt = async function (next) {
   try {
-    // this.password = await bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
   } catch (error) {
     throw new Error(error);
   }
@@ -18,9 +18,9 @@ const checkUserAvailable = function (user, next) {
 
 const verifyPassword = async function (rawPass) {
   try {
-    // if (!(await bcrypt.compare(rawPass, this.password))) {
-    //   throw new Error("Invalid Password");
-    // }
+    if (!(await bcrypt.compare(rawPass, this.password))) {
+      throw new Error("Invalid Password");
+    }
   } catch (error) {
     throw new Error(error.message);
   }
